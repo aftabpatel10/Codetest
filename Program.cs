46bbb72f9a0afd7a3b88e8c3090b91e3fc36c7f3
aftabpatel10 <aftabpatel3107@gmail.com>
2022-10-31 09:04:40 +0530

    Adding Testrepo

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+namespace HellowordDemo
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My Name is Aftab");
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ programe.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace HellowordDemo
+{
+    class programe
+    {
+        static void main(string[] args)
+        { console.WriteLine("My Name is Aftab");
+            console.ReadLine();
+        }
+
+    }
